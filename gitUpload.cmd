c981963409adefcf2b56ad09e3e20c674cbcd7e4
paul magu <pmagu@yahoo.com>
2019-08-10 12:27:29 -0700

    netlify changes

--- gitUpload.cmd
+++ gitUpload.cmd
@@ -1,7 +1,6 @@
 echo "# FantasyLotteryServer NODE::REST, WEbSockets API test" >> README.md
 git init
 git add .
-git add README.md
 git commit -m "first commit"
 git remote add origin https://github.com/ipaulmagu/FantasyLotteryServer.git
 git push -u origin master
